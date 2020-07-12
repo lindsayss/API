66fa86dbb11f13d01a198678b1db3555fb396a0b
Lindsayss <67104945+lindsayss@users.noreply.github.com>
2020-07-12 21:35:13 +0800

    Update default_test.rb

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,20 +5,20 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-describe package('apache2') do
+describe package('httpd') do
   it { should be_installed }
 end
 
 describe file('/var/www/html/index.html') do
   it { should exist }
-  its('content') { should match(/Hello ChefConf2019!!/) } 
+  its('content') { should match(/Hello Lindsay!!/) } 
 end
 
-describe upstart_service('apache2') do
+describe upstart_service('httpd') do
    it { should be_enabled }
    it { should be_running }
 end
 
 describe command('curl localhost') do
-    its('stdout') { should match(/Hello ChefConf2019!!/)}
+    its('stdout') { should match(/Hello Lindsay!!/)}
 end
